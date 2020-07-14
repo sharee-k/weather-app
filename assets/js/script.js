d604ade27ec7d5e8a5b8f468f6da780ed447c6e9
Sharee Burkeen <shareeburkeen@yahoo.com>
2020-07-14 13:24:08 -0500

    edited api link

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ var searchCityEl = document.querySelector(".search-box");
 //Get weather for current city search
 function getCurrentWeather() {
     var searchInput = document.querySelector(".search-input").value;
-    var api = 'http://api.openweathermap.org/data/2.5/weather?q=' + searchInput + '&units=imperial&appid=76db80d7cffbdb0aad58fd5a05192940';
+    var api = 'https://api.openweathermap.org/data/2.5/weather?q=' + searchInput + '&units=imperial&appid=76db80d7cffbdb0aad58fd5a05192940';
     console.log(api);
     fetch(api)
     .then(function(response) {
